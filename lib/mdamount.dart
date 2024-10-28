e7b2c76c1478dfe4be08bb6dfd901b695b6a59b4
Oleh Marchenko <oleh.marchenko.95@gmail.com>
2024-10-28 10:43:42 +0200

    added (c)

--- lib/mdamount.dart
+++ lib/mdamount.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2024, the MarchDev Toolkit project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 export 'package:decimal/decimal.dart';
 
 export 'src/exceptions.dart';
